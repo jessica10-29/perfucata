bfaa8a99c591f57355185cb3a7c35ca1bc7d1938
jessica10-29 <seguraordonezpaola@gmail.com>
2026-04-12 14:07:14 -0500

    configuracion

--- db_connect.php
+++ db_connect.php
@@ -3,10 +3,22 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "perfucata";
+// Detectar entorno (localhost vs servidor en línea)
+$isLocalhost = ($_SERVER['HTTP_HOST'] === 'localhost' || strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== false);
+
+if ($isLocalhost) {
+    // Configuración para LOCALHOST
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "perfucata";
+} else {
+    // Configuración para INFINITYFREE (actualiza con tus credenciales reales)
+    $servername = "localhost"; // InfinityFree usa localhost
+    $username = "if0_41640364_perfucata"; // Reemplaza con tu usuario
+    $password = "tu_contraseña_aqui"; // Reemplaza con tu contraseña
+    $dbname = "if0_41640364_perfucata"; // Reemplaza con tu nombre de BD
+}
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
